037d0450be269469e6df40c89758d515a097b8ab
Matheus Ferreira <matheusguiferreira@hotmail.com>
2020-09-17 13:49:25 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/ferreira/cursomc/services/CategoriaService.java
+++ src/main/java/com/ferreira/cursomc/services/CategoriaService.java
@@ -16,7 +16,7 @@ public class CategoriaService {
 	@Autowired // dependencia automaticamente instanciada pelo spring
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		 Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: "+ id+ ", Tipo: "+Categoria.class.getName()));
@@ -24,6 +24,11 @@ public class CategoriaService {
 	
 	public Categoria inserir(Categoria obj) {
 		obj.setId(null); // Operação para garantir que realmente estamos inserindo um objeto novo, caso contrário é um update
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
 		return repo.save(obj);
 	}
 }
--- src/main/java/com/ferreira/cursomc/services/PedidoService.java
+++ src/main/java/com/ferreira/cursomc/services/PedidoService.java
@@ -16,7 +16,7 @@ public class PedidoService {
 	@Autowired // dependencia automaticamente instanciada pelo spring
 	private PedidoRepository repo;
 	
-	public Pedido buscar(Integer id) {
+	public Pedido find(Integer id) {
 		 Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: "+ id+ ", Tipo: "+Pedido.class.getName()));
